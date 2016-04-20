b42c418443516ef147b120cb40252c1ce8d90013
Dimitry Volker <dimitry.volker@gmail.com>
2016-04-20 12:55:21 +0200

    Made IntensityImageStudent with needed functions

--- source/ExternalDLL/ExternalDLL/IntensityImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/IntensityImageStudent.cpp
@@ -1,44 +1,64 @@
 #include "IntensityImageStudent.h"
+#include <iostream>
 
 IntensityImageStudent::IntensityImageStudent() : IntensityImage() {
 	int throwError = 0, e = 1 / throwError; //Throws error without the need to include a header
 	//TODO: Nothing
 }
 
-IntensityImageStudent::IntensityImageStudent(const IntensityImageStudent &other) : IntensityImage(other.getWidth(), other.getHeight()) {
+IntensityImageStudent::IntensityImageStudent(const IntensityImageStudent &other) : IntensityImage(other.getWidth(), other.getHeight()) , sWidth(other.getWidth()), sHeight(other.getHeight()){
 	int throwError = 0, e = 1 / throwError;
-	//TODO: Create a copy from the other object
+
 }
 
-IntensityImageStudent::IntensityImageStudent(const int width, const int height) : IntensityImage(width, height) {
+IntensityImageStudent::IntensityImageStudent(const int width, const int height) : IntensityImage(width, height), sWidth(width), sHeight(height) {
 	int throwError = 0, e = 1 / throwError;
-	//TODO: Initialize pixel storage
+	createStorage();
 }
 
 IntensityImageStudent::~IntensityImageStudent() {
 	int throwError = 0, e = 1 / throwError;
 	//TODO: delete allocated objects
+	deleteStorage();
 }
 
 void IntensityImageStudent::set(const int width, const int height) {
 	IntensityImage::set(width, height);
 	int throwError = 0, e = 1 / throwError;
+
+	deleteStorage(); sWidth = width; sHeight = height;
+	createStorage();
 	//TODO: resize or create a new pixel storage (Don't forget to delete the old storage)
 }
 
 void IntensityImageStudent::set(const IntensityImageStudent &other) {
 	IntensityImage::set(other.getWidth(), other.getHeight());
 	int throwError = 0, e = 1 / throwError;
+
+	deleteStorage(); sWidth = other.getWidth(); sHeight = other.getHeight();
+	createStorage();
+
+	for (int y = 0; y < sHeight; ++y){
+		for (int x = 0; x < sWidth; ++x)
+			intensityStorage[y][x] = other.getPixel(x, y);
+	}
 	//TODO: resize or create a new pixel storage and copy the object (Don't forget to delete the old storage)
 }
 
 void IntensityImageStudent::setPixel(int x, int y, Intensity pixel) {
 	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
+
+	if (x > sWidth || y > sHeight)
+		std::cout << "Pixel out of range"; return;
+
+	intensityStorage[y][x] = pixel;
 }
 
 void IntensityImageStudent::setPixel(int i, Intensity pixel) {
 	int throwError = 0, e = 1 / throwError;
+	Intensity *p = &intensityStorage[0][0];
+	*(p + i) = pixel;
+
 	/*
 	* TODO: set pixel i in "Row-Major Order"
 	*
@@ -65,11 +85,36 @@ void IntensityImageStudent::setPixel(int i, Intensity pixel) {
 Intensity IntensityImageStudent::getPixel(int x, int y) const {
 	int throwError = 0, e = 1 / throwError;
 	//TODO: no comment needed :)
-	return 0;
+
+	if (x > sWidth || y > sHeight)
+		std::cout << "Pixel out of range"; return 0;
+
+	return intensityStorage[y][x];
 }
 
 Intensity IntensityImageStudent::getPixel(int i) const {
 	int throwError = 0, e = 1 / throwError;
 	//TODO: see setPixel(int i, RGB pixel)
-	return 0;
+
+	if (i > (sWidth * sHeight))
+		std::cout << "Pixel out of range"; return 0;
+
+	Intensity *p = &intensityStorage[0][0];
+	return *(p + i);
+}
+
+void IntensityImageStudent::createStorage(){
+	if (intensityStorage != nullptr)
+		deleteStorage();
+
+	intensityStorage = new Intensity*[sHeight];
+	for (int i = 0; i < sHeight; ++i)
+		intensityStorage[i] = new Intensity[sWidth];
+
+}
+
+void IntensityImageStudent::deleteStorage(){
+	for (int i = 0; i < sHeight; ++i)
+		delete[] intensityStorage[i];
+	delete[] intensityStorage;
 }
